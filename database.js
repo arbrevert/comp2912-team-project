4af1c0095f6ae067d6f8ade968169baf4c5447f5
Jason Tao <jtao@ddswireless.com>
2021-03-24 23:47:32 -0700

    Mar 25 2021

--- database.js
+++ database.js
@@ -10,11 +10,20 @@
 let Database = {
     cindy: {
         email:"cindy@gmail.com", src:"local", password:"cindy",
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+        reminders: [
+            {id: 1, title: "abc", description: "abcabc", completed: false},
+            {id: 2, title: "abc", description: "abcabc", completed: false, dueDate:"2021-04-01"},
+        ]
     },
     2: {
-        email:"alex@gmail.com", src:"local",
-        reminders: []
+        email:"alex@gmail.com", src:"local", password:"alex",
+        reminders: [
+            {id: 1, title: "alex1", description: "alex1 reminder", completed: false, dueDate:"2021-04-01"},
+            {id: 2, title: "alex2", description: "alex2 reminder", completed: false, dueDate:"2021-04-01"},
+            {id: 3, title: "alex3", description: "alex3 reminder", completed: false, dueDate:"2021-04-01"},
+            {id: 4, title: "alex4", description: "alex4 reminder", completed: false, dueDate:"2021-04-01"},
+            {id: 5, title: "alex5", description: "alex5 reminder", completed: false, dueDate:"2021-04-01"},
+        ]
     } 
 }
 
